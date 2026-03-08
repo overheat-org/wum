2666793c868ee24c6432bb591677ed713703e1a5
nx <nicolasestevaolima104@gmail.com>
2026-03-08 03:49:48 -0300

    feat: update index.ts to include additional patterns and exports

--- packages/wum/src/index.ts
+++ packages/wum/src/index.ts
@@ -2,3 +2,4 @@ export * from './structures/CommandContainer.js';
 export { Storage, autoincrement } from './structures/Storage.js';
 export { WumClient } from './structures/Client.js';
 export * from './structures/Stored.js';
+export * from 'discord.js';
